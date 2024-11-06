8149491f4db1e13e0ff52ce7241f0367ab0efe49
Andrei Litvin <andy314@gmail.com>
2024-11-06 17:48:11 -0500

    Update plugins

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env nu
+
 echo "Installing SKIM plugin..."
 cargo install nu_plugin_skim
 plugin add ~/.cargo/bin/nu_plugin_skim
+
+echo "Installing clipboard plugin"
+cargo install nu_plugin_clipboard
+plugin add ~/.cargo/bin/nu_plugin_clipboard
+
+# NOT INSTALLED: not yet updated to latest nushell
+# echo "Installing compress plugin"
+# cargo install nu_plugin_compress
+# plugin add ~/.cargo/bin/nu_plugin_compress
